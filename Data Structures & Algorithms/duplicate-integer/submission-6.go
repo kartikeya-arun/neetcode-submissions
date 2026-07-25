5293d321f814b13a52462f2d464cfe6c2eefc701
Kartikeya <52625395+kartikeya-arun@users.noreply.github.com>
2026-07-25 18:40:53 +0530

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.go
@@ -0,0 +1,11 @@
+func hasDuplicate(nums []int) bool {
+    seen := make(map[int]bool)
+    for _, num := range(nums){
+        if seen[num]{
+            return true
+        }else{
+            seen[num] = true
+        }
+    }
+    return false
+}
